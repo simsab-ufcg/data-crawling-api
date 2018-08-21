81bb9d0df9c7995108081af7be4a2d916aa8d0be
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-08-21 14:35:02 -0300

    authentication and authorization template updated

--- model/user.model.js
+++ model/user.model.js
@@ -10,5 +10,5 @@ var client = new pg.Client(connectionURL);
 
 client.connect();
 
-var query = client.query("CREATE TABLE user(id SERIAL PRIMARY KEY, username VARCHAR(30) NOT NULL UNIQUE, password VARCHAR(30) NOT NULL)");
+var query = client.query("CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username VARCHAR(30) NOT NULL UNIQUE, password VARCHAR(30) NOT NULL)");
 query.on('end', () => {client.end});
