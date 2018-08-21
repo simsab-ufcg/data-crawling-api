b3cc45ce690a159982d817376006526ecc0cd1c8
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-08-21 13:31:16 -0300

    db folder deleted

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,10 +1,21 @@
+/**
+ * Definition of the api routes.
+ * 
+ * @author Ana Silva
+ */
 var express = require('express');
 var router = express.Router();
 
+/**
+ * route that loads the root of the api 
+ */
 router.get('/', function(req, res) {
   res.send('API route Works');
 });
 
+/**
+ *  route that loads all avaliable datasets
+ */
 router.get('/dataset', function(req, res) {
   res.send('Dataset works');
 });
