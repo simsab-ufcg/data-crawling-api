3b5c9028a51227ea5559c21fd0852406a471d910
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-08-21 17:00:05 -0300

    Changing crawler structure

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -5,49 +5,27 @@
  */
 var express = require('express');
 var router = express.Router();
-var JSSoup = require('jssoup').default;
-var axios = require('axios');
-const URL = 'http://transferserver.insa.gov.br'
+var crawler = require('../utils/crawler.util');
 
-var crawlerPage = (path, response) => {
-  axios.get(URL + path).then((res) =>{
-    var soup = new JSSoup(res.data);
-    var html = soup.nextElement.nextElement;
-    var body = html.find('body');
-    var tr = body.findAll('tr');
-    var tableSize = tr.length;
-    var ls = [];
-    var td0 = tr[0].findAll('th');
-    var names = td0.map(element => element.text);
-    names[0] = 'isDirectory';
-    for(var i = 2; i < tableSize - 1; i+=1){
-      var td = tr[i].findAll('td');
-      var result = {};
-      result[names[0]] = td[0].nextElement.attrs['alt'] == '[DIR]';
-      for(var j = 1; j < td.length; j+= 1){
-        var data = td[j];
-        result[names[j]] = td[j].text; 
-      }
-      ls.push(result);
-    }
-    response.send(ls);
-  }).catch((err) =>{
-    console.log(err.message);
-  });
-}
 
 /**
  * route that loads the root of the api 
  */
 router.get('/', function(req, res) {
-  crawlerPage('/', res);
+  res.status(400).send('Hello world');
 });
 
 /**
  *  route that loads all avaliable datasets
  */
 router.get('/dataset', function(req, res) {
-  res.send('Dataset works');
+  crawler.crawlerPage('/', res);
 });
+
+router.post('/dataset', function(req, res) {
+  console.log(req.body);
+  crawler.crawlerPage('/' + req.body.path, res);
+});
+
 
 module.exports = router;
---
+++ utils/crawler.util.js
@@ -0,0 +1,33 @@
+var JSSoup = require('jssoup').default;
+var axios = require('axios');
+const URL = 'http://transferserver.insa.gov.br'
+
+exports.crawlerPage = (path, response) => {
+  axios.get(URL + path).then((res) =>{
+    var soup = new JSSoup(res.data);
+    var html = soup.nextElement.nextElement;
+    var body = html.find('body');
+    var tr = body.findAll('tr');
+    var tableSize = tr.length;
+    var ls = [];
+    var td0 = tr[0].findAll('th');
+    var names = td0.map(element => element.text);
+    names[0] = 'isDirectory';
+    for(var i = 2; i < tableSize - 1; i+=1){
+      var td = tr[i].findAll('td');
+      var result = {};
+      result[names[0]] = td[0].nextElement.attrs['alt'] == '[DIR]';
+      for(var j = 1; j < td.length; j+= 1){
+        var data = td[j];
+        result[names[j]] = td[j].text.trim(); 
+      }
+      if(result.Description == '&nbsp;')
+        result.Description = '';
+      if(result.Name != "Parent Directory")
+        ls.push(result);
+    }
+    response.send(ls);
+  }).catch((err) =>{
+    console.log(err.message);
+  });
+}
