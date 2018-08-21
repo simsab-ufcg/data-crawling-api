a55b298ab8b5508dc579aa552dd5d50533741547
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-08-21 13:46:41 -0300

    user model updated

--- model/user.model.js
+++ model/user.model.js
@@ -2,4 +2,13 @@
  * @author Ana Silva
  */
 
- 
+ var pg = require("pg");
+
+ var connectionURL = process.env.connectionURL || 'postgres://localhost:5432/simsab-dataset';
+
+var client = new pg.Client(connectionURL);
+
+client.connect();
+
+var query = client.query("CREATE TABLE user(id SERIAL PRIMARY KEY, username VARCHAR(30) NOT NULL UNIQUE, password VARCHAR(30) NOT NULL)");
+query.on('end', () => {client.end});
