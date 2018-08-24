ea940c76f515a55e1d8b16e39487c3f7f0cc2073
Ana Silva <anasilva@lsd.ufcg.edu.br>
2018-08-24 14:41:00 -0300

    libraries required for authentication added on project

--- app.js
+++ app.js
@@ -19,12 +19,30 @@ var express = require("express");
 var morgan = require("morgan");
 var bodyParser = require("body-parser");
 var port = process.env.PORT || 6000;
+var request = require("request");
+var session = require("express-session");
+var passport = require("passport");
+var path = require("path");
+var flash = require("connect-flash");
 require('./config/auth.config');
 
 /**
  * Loads all the api specific routes
  */
 var api = require('./routes/api.routes');
+
+/**
+ * Loads other dependencies.
+ */
+app.use(require('cookie-parser')());
+const expressSession = require("express-session");
+app.use(expressSession({secret: process.env.SECRET_KEY}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use(session({secret: process.env.ANOTHER_SECRET_KEY}));
+app.set('view engine', 'pug');
+app.set('view options', { layout: false });
 
 var app = express();
 
