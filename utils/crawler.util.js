d5d517aa8ea2273df2672cc9070aa5759f81f7da
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2018-08-27 10:58:14 -0300

    Working without / in query param

--- utils/crawler.util.js
+++ utils/crawler.util.js
@@ -16,59 +16,59 @@ var crawler = require("../utils/crawler.util.js");
  * @param {Object} response 
  */
 exports.crawlerPage = (path, request, response) => {
-  axios.get(URL + path).then((res) =>{
+  axios.get(URL + path).then((res) => {
     var soup = new JSSoup(res.data);
 
     // Soup will find html body
     var html = soup.nextElement.nextElement;
     var body = html.find('body');
-    
+
     // Create list with all directory rows.
     var tr = body.findAll('tr');
     var tableSize = tr.length;
     var ls = [];
     var td0 = tr[0].findAll('th');
     var names = [];
-    for(var i = 1; i < td0.length; i += 1){
+    for (var i = 1; i < td0.length; i += 1) {
       names.push(td0[i].nextElement.text);
     }
-    for(var i = 2; i < tableSize - 1; i+=1){
+    for (var i = 2; i < tableSize - 1; i += 1) {
       var td = tr[i].findAll('td');
       var result = {};
-      
-      for(var j = 1; j < td.length; j+= 1){
+
+      for (var j = 1; j < td.length; j += 1) {
         var data = td[j];
-        result[names[j - 1]] = td[j].text.trim(); 
+        result[names[j - 1]] = td[j].text.trim();
       }
 
-      if(result.Description == '&nbsp;'){
+      if (result.Description == '&nbsp;') {
         result.Description = '';
       }
 
-      if(td[0].nextElement.attrs['alt'] != '[DIR]'){
-        result.Download = URL + path + result.Name;  
+      if (td[0].nextElement.attrs['alt'] != '[DIR]') {
+        result.Download = URL + path + result.Name;
       }
 
-      if(result.Name != "Parent Directory")
-      ls.push(result);
-    }
-    
+      if (result.Name != "Parent Directory")
+        ls.push(result);
+    } 
+
     var search_dataset = request.query.dataset;
 
-    if(search_dataset === undefined || path !== ""){
-      response.status(200).send(ls);  
-    }else{
-      
-      ls = ls.filter((element) => (element.Name == search_dataset));
+    if (search_dataset === undefined || path !== "") {
+      response.status(200).send(ls);
+    } else {
 
-      if(ls.length == 0){
+      ls = ls.filter((element) => (element.Name == search_dataset || element.Name == search_dataset + '/'));
+
+      if (ls.length == 0) {
         response.status(404).send("Dataset not found.");
-      }else{
+      } else {
         crawler.crawlerPage(ls[0].Name, request, response);
       }
 
     }
-  }).catch((err) =>{
+  }).catch((err) => {
     console.log(err.message);
     response.status(503).send(err.message);
   });
